815ded759527832c6c960b4b6f7e56e8643b9338
Eamonn Webster <eweb@qstream.com>
2015-04-06 21:18:06 +0100

    #0008 changes from wacc

--- ecopy.rb
+++ ecopy.rb
@@ -14,8 +14,11 @@
 # 25th Mar 2015  eweb     #0007 Reformat
 # 25th Mar 2015  eweb     #0007 Classify
 # 25th Mar 2015  eweb     #0007 Avoid infinite recursion
+# 28th Mar 2015  eweb     #0008 ecopy.rb from bin
+# 28th Mar 2015  eweb     #1723 Adapted for use in app
+#  6th Apr 2015  eweb     #0007 tidy up
+#  6th Apr 2015  eweb     #0007 break once copied or removed
 #
-require 'FileUtils'
 
 # ruby replacement for ecopy.exe
 
@@ -23,7 +26,6 @@ require 'FileUtils'
 # -d destination folder
 # -r recursive
 # -n[-] only newer
-# -w windiff
 # -Y pattern
 # -N pattern
 
@@ -40,24 +42,51 @@ class AppIO
   def puts(msg)
     Kernel::puts(msg)
   end
+
   def print(msg)
     Kernel::print(msg)
   end
+
   def get_key
     STDIN.gets[0..0]
   end
+
   def get_response(prompt)
     print prompt
     get_key
   end
-  def system(cmd)
-    Kernel::system cmd
+
+  def diff_cmd(dst, src)
+    "diff -u '#{dst}' '#{src}'"
+  end
+
+  def ediff_cmd(dst, src)
+    "emacs --eval \"(ediff-files \\\"#{src}\\\" \\\"#{dst}\\\")\""
+  end
+
+  def view_file(file)
+    cmd = "less '#{file}'"
+    puts "cmd: #{cmd}"
+    system cmd
+  end
+
+  def diff_files(destination, source)
+    cmd = diff_cmd(destination, source)
+    puts "cmd: #{cmd}"
+    system cmd
+    # puts ""
+  end
+
+  def ediff_files(destination, source)
+    cmd = ediff_cmd(destination, source)
+    puts "cmd: #{cmd}"
+    system cmd
   end
 end
 
 class ECopy
-  def initialize
-    @io = AppIO.new
+  def initialize(io=nil)
+    @io = io || AppIO.new
   end
 
   def puts(msg)
@@ -72,10 +101,6 @@ class ECopy
     @io.get_response(prompt)
   end
 
-  def system(cmd)
-    @io.system(cmd)
-  end
-
   def match_file_one(file, opt)
     #puts( "match_file_one(#{file},#{opt})" )
     if opt
@@ -83,7 +108,7 @@ class ECopy
     end
   end
 
-  def match_file(file, options)
+  def match_file(file)
     otherwise = true
     inc = true
     if options
@@ -93,11 +118,11 @@ class ECopy
         elsif opt =~ /^-/
         else
           if inc
-            puts "opt = [#{opt}] setting otherwise to false\n"
+            puts "opt = [#{opt}] setting otherwise to false"
             otherwise = false
           end
           if match_file_one(file, opt)
-            puts "match_file( #{file}, #{opt} ) ==> #{inc}\n" if options.find { |x| x == "-v" }
+            puts "match_file( #{file}, #{opt} ) ==> #{inc}" if verbose
             return inc
           end
           inc = true
@@ -105,18 +130,18 @@ class ECopy
       end
     end
     # no explicit match
-    puts "match_file( #{file} ) ==> #{otherwise}\n" if options.find { |x| x == "-v" }
+    puts "match_file( #{file} ) ==> #{otherwise}" if verbose
     otherwise
   end
 
-  def contents_of_folder(folder, options)
+  def contents_of_folder(folder)
     contents = Dir.glob("#{folder}/*").collect { |x| x.sub("#{folder}/", "") }.sort
     contents.reject do |file|
-      !match_file(file, options) && !File.directory?("#{folder}/#{file}")
+      !match_file(file) && !File.directory?("#{folder}/#{file}")
     end
   end
 
-  def create_dir(dir, options)
+  def create_dir(dir)
     if File.directory?(dir)
     else
       prompt = "Create directory #{dir}? (y/n/s)"
@@ -171,146 +196,156 @@ class ECopy
     File.rename(source, destination)
   end
 
-  def process_file(prompt, source, destination, options, switches, reverse=nil)
+  def create_dir_if_needed(prompt, dest_dir, switches)
+    if !switches['N'] && !File.directory?(dest_dir) && !File.file?(dest_dir)
+      #puts "switches #{switches}"
+      # if the directory doesn't exist
+      print prompt
+      dir_ans = create_dir(dest_dir)
+      if dir_ans.upcase == dir_ans
+        #puts "Setting switch #{dir_ans}"
+        switches[dir_ans] = true
+      end
+      if dir_ans == 'q'
+        dir_ans
+      elsif dir_ans.downcase == 'n'
+        dir_ans.downcase
+      elsif dir_ans.downcase == 's'
+        dir_ans.downcase
+      end
+    end
+  end
+
+  def process_file_ans(prompt, switches)
+    if switches['Y']
+      ans = 'y'
+    elsif switches['C']
+      ans = 'c'
+    elsif switches['N']
+      ans = 'n'
+    elsif switches['B']
+      ans = 'b'
+    elsif switches['R']
+      ans = 'r'
+    else
+      ans = get_response(prompt)
+    end
+    if ans == ans.upcase
+      switches[ans] = true
+      #puts "have added #{ans} to #{switches}"
+      ans = ans.downcase
+    end
+    ans
+  end
+
+  def process_file_copy(source, destination)
+    dest_dir = File.dirname(destination)
+    if !File.directory?(dest_dir) && !File.file?(dest_dir)
+      create_dir(dest_dir)
+    end
+    if File.directory?(dest_dir)
+      copy_file(source, destination, true)
+    end
+  end
+
+  def process_file_back(source, destination)
+    if !File.file?(destination)
+      puts "Can't copy back destination #{destination} doesn't exist"
+    else
+      copy_file(destination, source, true)
+    end
+  end
+
+  def process_file_remove(source, destination, reverse)
+    if reverse
+      puts "FileUtils.remove_file(#{destination},true)"
+      FileUtils.remove_file(destination, true)
+    else
+      puts "FileUtils.remove_file(#{source},true)"
+      FileUtils.remove_file(source, true)
+    end
+  end
+
+  def process_file_view(source, destination)
+    if File.file?(destination)
+      view_file(destination)
+    elsif File.file?(source)
+      view_file(source)
+    end
+  end
+
+  def process_file(prompt, source, destination, switches, reverse=nil)
     if File.directory?(source)
-      process_folder(source, destination, options, switches)
+      process_folder(source, destination, switches)
     elsif File.directory?(destination)
-      process_folder(source, destination, options, switches)
+      process_folder(source, destination, switches)
     else
       while true
         dest_dir = File.dirname(destination)
-        ans = false
-        if !switches['N'] && !File.directory?(dest_dir) && !File.file?(dest_dir)
-          #puts "switches #{switches}\n"
-          # if the directory doesn't exist
-          print prompt
-          dir_ans = create_dir(dest_dir, options)
-          if dir_ans.upcase == dir_ans
-            #print "Setting switch #{dir_ans}\n"
-            switches[dir_ans] = true
-          end
-          if dir_ans == 'q'
-            return dir_ans
-          elsif dir_ans.downcase == 'n'
-            return dir_ans.downcase
-          elsif dir_ans.downcase == 's'
-            return dir_ans.downcase
-          end
-        end
-        if ans
-        elsif switches['Y']
-          ans = 'y'
-        elsif switches['C']
-          ans = 'c'
-        elsif switches['N']
-          ans = 'n'
-        elsif switches['B']
-          ans = 'b'
-        elsif switches['R']
-          ans = 'r'
-        else
-          ans = get_response(prompt)
-        end
-        if ans == ans.upcase
-          switches[ans] = true
-          #puts "have added #{ans} to #{switches}\n"
-          ans = ans.downcase
-        end
-        if ans == 'c' || ans == 'y'
-          dest_dir = File.dirname(destination)
-          if !File.directory?(dest_dir) && !File.file?(dest_dir)
-            create_dir(dest_dir, options)
-          end
-          if File.directory?(dest_dir)
-            copy_file(source, destination, true)
-          end
-          break
-        elsif ans == 'b'
-          if !File.file?(destination)
-            puts "Can't copy back destination #{destination} doesn't exist"
+        dir_ans = create_dir_if_needed(prompt, dest_dir, switches)
+        return dir_ans if dir_ans
+
+        ans = process_file_ans(prompt, switches)
+
+        case ans
+          when 'c', 'y'
+            process_file_copy(source, destination)
+            return
+          when 'b'
+            process_file_back(source, destination)
+            return
+          when 'r'
+            process_file_remove(source, destination, reverse)
+            return
+          when 'd'
+            diff_files(destination, source)
+          when 'e'
+            ediff_files(destination, source)
+          when 'v'
+            process_file_view(source, destination)
+          when 'q'
+            return ans
           else
-            copy_file(destination, source, true)
-          end
-          break
-        elsif ans == 'r'
-          if reverse
-            puts "FileUtils.remove_file(#{destination},true)"
-            FileUtils.remove_file(destination, true)
-          else
-            puts "FileUtils.remove_file(#{source},true)"
-            FileUtils.remove_file(source, true)
-          end
-          break
-        elsif ans == 'd'
-          cmd = diff_cmd(destination, source)
-          puts "d: #{cmd}\n"
-          system cmd
-        elsif ans == 'e'
-          cmd = ediff_cmd(destination, source)
-          puts "e: #{cmd}\n"
-          system cmd
-        elsif ans == 'v'
-          if File.file?(destination)
-            cmd = "less '#{destination}'"
-            puts "cmd: #{cmd}\n"
-            system cmd
-          elsif File.file?(source)
-            cmd = "less '#{source}'"
-            puts "cmd: #{cmd}\n"
-            system cmd
-          end
-        elsif ans == 'q'
-          return ans
-        else
-          break
+            return
         end
       end
     end
   end
 
-  def diff_cmd(dst, src)
-    "diff -u '#{dst}' '#{src}'"
+  def view_file(file)
+    @io.view_file(file)
   end
 
-  def ediff_cmd(dst, src)
-    "emacs --eval \"(ediff-files \\\"#{src}\\\" \\\"#{dst}\\\")\""
+  def diff_files(file1, file2)
+    @io.diff_files(file1, file2)
   end
 
-  def compare_files(source, destination, options)
-    puts "compare_files(#{source},#{destination})\n" if options.find { |x| x == "-v" }
+  def ediff_files(file1, file2)
+    @io.ediff_files(file1, file2)
+  end
+
+  def compare_files(source, destination)
+    puts "compare_files(#{source},#{destination})" if verbose
     only_newer = !options.find { |x| x == "-n-" }
     if only_newer
       if File.mtime(source) <= File.mtime(destination)
-        puts "compare_files(File.mtime(#{source}) <= File.mtime(#{destination})\n" if options.find { |x| x == "-v" }
+        puts "compare_files(File.mtime(#{source}) <= File.mtime(#{destination})" if verbose
         return true
       end
-      puts "src: #{File.mtime(source)} #{source}\ndst: #{File.mtime(destination)} #{destination}\n" if options.find { |x| x == "-v" }
+      puts "src: #{File.mtime(source)} #{source}\ndst: #{File.mtime(destination)} #{destination}" if verbose
     end
 
     if FileUtils.compare_file(source, destination)
-      puts "FileUtils.compare_file(#{source},#{destination}) => true\n" if options.find { |x| x == "-v" }
+      puts "FileUtils.compare_file(#{source},#{destination}) => true" if verbose
       true
     else
-      puts "FileUtils.compare_file(#{source},#{destination}) => false\n" if options.find { |x| x == "-v" }
-      cmd = diff_cmd(destination, source)
-      puts "cmd: #{cmd}\n"
-      system cmd
-      puts "\n"
+      puts "FileUtils.compare_file(#{source},#{destination}) => false" if verbose
+      diff_files(destination, source)
       false
     end
   end
 
-  def process_folder(source, destination, options, switches)
-    puts source
-    source = source[0..-2] if source.end_with?('/')
-    destination = destination[0..-2] if destination.end_with?('/')
-    local_switches = switches.clone
-    source_files = contents_of_folder(source, options)
-    destination_files = contents_of_folder(destination, options)
-    adds = source_files - destination_files
-    deletes = destination_files - source_files
-    compares = destination_files - (destination_files - source_files)
-
+  def process_folder_mismatch(adds, deletes, compares, source, destination)
     adds.each do |add|
       deletes.each do |del|
         if del.downcase == add.downcase
@@ -318,87 +353,140 @@ class ECopy
           ans = get_response prompt
           if ans == 'b'
             rename_file "#{destination}/#{del}", "#{destination}/#{add}"
-            adds = adds - [add]
-            deletes = deletes - [del]
-            compares = compares + [add]
+            adds.delete(add)
+            deletes.delete(del)
+            compares.push(add)
           elsif ans == 'a'
             rename_file "#{source}/#{add}", "#{source}/#{del}"
-            adds = adds - [add]
-            deletes = deletes - [del]
-            compares = compares + [del]
+            adds.delete(add)
+            deletes.delete(del)
+            compares.push(del)
           end
         end
       end
     end
+  end
 
+  def process_folder_adds(source, destination, adds, local_switches)
     puts "Files in #{source} but not in #{destination}" unless adds.empty?
     adds.each do |file|
+      source_file = "#{source}/#{file}"
+      destination_file = "#{destination}/#{file}"
       if file =~ /\(.*conflicted copy.*\)/
         stem = file.sub(/ \(.*conflicted copy.*\)/, '')
-        ans = process_file("add file #{source}/#{file} #{source}/#{stem}? (c/r/d/e/v)", "#{source}/#{file}", "#{source}/#{stem}", options, local_switches)
+        ans = process_file("add file #{source_file} #{source}/#{stem}? (c/r/d/e/v)", source_file, "#{source}/#{stem}", local_switches)
       else
-        ans = process_file("add file #{source}/#{file} #{destination}/#{file}? (c/r/v)", "#{source}/#{file}", "#{destination}/#{file}", options, local_switches)
+        ans = process_file("add file #{source_file} #{destination_file}? (c/r/v)", source_file, destination_file, local_switches)
       end
-      #puts "local_switches #{local_switches}\n"
+      #puts "local_switches #{local_switches}"
       return ans if ans == 'q'
-      return if ans == 's'
-      return if ans == 'n' # no to create directory not no to a file
+      return ans if ans == 's'
+      return ans if ans == 'n' # no to create directory not no to a file
     end
+    nil
+  end
+
+  def process_folder_deletes(source, destination, deletes, local_switches)
     puts "Files not in #{source} but in #{destination}" unless deletes.empty?
     deletes.each do |file|
-      ans = process_file("remove file #{destination}/#{file}? (r/b/v)", "#{source}/#{file}", "#{destination}/#{file}", options, local_switches, true)
+      source_file = "#{source}/#{file}"
+      destination_file = "#{destination}/#{file}"
+      ans = process_file("remove file #{destination_file}? (r/b/v)", source_file, destination_file, local_switches, true)
       return ans if ans == 'q'
     end
+    nil
+  end
+
+  def process_folder_compares(source, destination, compares, local_switches)
     compares.each do |file|
-      if File.file?("#{source}/#{file}") && File.file?("#{destination}/#{file}")
-        if !compare_files("#{source}/#{file}", "#{destination}/#{file}", options)
-          ans = process_file("copy file #{source}/#{file}? (c/r/b/d/e)", "#{source}/#{file}", "#{destination}/#{file}", options, local_switches)
+      source_file = "#{source}/#{file}"
+      destination_file = "#{destination}/#{file}"
+      if File.file?(source_file) && File.file?(destination_file)
+        if !compare_files(source_file, destination_file)
+          ans = process_file("copy file #{source_file}? (c/r/b/d/e)", source_file, destination_file, local_switches)
           return ans if ans == 'q'
         end
-      elsif File.directory?("#{source}/#{file}") && File.directory?("#{destination}/#{file}")
-        ans = process_folder("#{source}/#{file}", "#{destination}/#{file}", options, local_switches)
+      elsif File.directory?(source_file) && File.directory?(destination_file)
+        ans = process_folder(source_file, destination_file, local_switches)
         return ans if ans == 'q'
-      elsif File.directory?("#{source}/#{file}") && File.file?("#{destination}/#{file}")
-        if switches['v']
-          puts "#{source}/#{file} shadowed by file #{destination}/#{file}\n"
+      elsif File.directory?(source_file) && File.file?(destination_file)
+        if verbose
+          puts "#{source_file} shadowed by file #{destination_file}"
         end
-      elsif File.file?("#{source}/#{file}") && File.directory?("#{destination}/#{file}")
-        if switches['v']
-          puts "#{source}/#{file} is shadow for directory #{destination}/#{file}\n"
+      elsif File.file?(source_file) && File.directory?(destination_file)
+        if verbose
+          puts "#{source_file} is shadow for directory #{destination_file}"
         end
       end
+    end
+  end
+
+  def process_folder(source, destination, switches)
+    puts source
+    source = source[0..-2] if source.end_with?('/')
+    destination = destination[0..-2] if destination.end_with?('/')
+    local_switches = switches.clone
+
+    source_files = contents_of_folder(source)
+    destination_files = contents_of_folder(destination)
+
+    adds = source_files - destination_files
+    deletes = destination_files - source_files
+    compares = destination_files - (destination_files - source_files)
+
+    process_folder_mismatch(adds, deletes, compares, source, destination)
+
+    ans = process_folder_adds(source, destination, adds, local_switches)
+    return ans if ans == 'q'
+    return if ans == 's'
+    return if ans == 'n' # no to create directory not no to a file
+
+    ans = process_folder_deletes(source, destination, deletes, local_switches)
+    return ans if ans == 'q'
+    ans = process_folder_compares(source, destination, compares, local_switches)
+    return ans if ans == 'q'
+    nil
+  end
+
+  def verbose
+    options.find { |x| x == "-v" }
+  end
+
+  attr_accessor :options
+
+  def get_source(argv)
+    p = options.find_index('-s')
+    if p
+      source_root = options[p+1]
+      options[p..p+1] = []
+      source_root
+    end
+  end
+  def get_destination(argv)
+    p = options.find_index('-d')
+    if p
+      destination_root = options[p+1]
+      options[p..p+1] = []
+      destination_root
     end
   end
 
   def run(argv)
-    source_root = ""
-    destination_root = ""
-
-    puts("ecopy.rb " + argv.join(' '))
-    p = argv.find_index("-s")
-    if p
-      source_root = argv[p+1]
-      argv[p..p+1] = []
-    end
-    p = argv.find_index("-d")
-    if p
-      destination_root = argv[p+1]
-      argv[p..p+1] = []
-    end
-
-    puts "source_root: #{source_root}"
-    puts "destination_root: #{destination_root}"
+    self.options = argv
+    source_root = get_source(argv)
+    destination_root = get_destination(argv)
 
     unless File.directory?(source_root)
-      puts "source_root #{source_root} not found\n"
+      puts "source_root #{source_root} not found"
       return
     end
 
-    puts "options: " + argv.join(' ') + "\n"
-    switches = {}
-    process_folder(source_root, destination_root, argv, switches)
+    process_folder(source_root, destination_root, {})
   end
 
 end
 
-ECopy.new.run(ARGV)
+if $0 == __FILE__
+  require 'FileUtils'
+  ECopy.new.run(ARGV)
+end
