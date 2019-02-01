0ddb3f0207a3af58168836ac0cada435bb14cdbf
Eamonn Webster <eamonn.webster@gmail.com>
2019-02-01 20:44:57 +0000

    #0008 iTunes not on Transcend

--- import_music.rb
+++ import_music.rb
@@ -3,17 +3,21 @@
 #
 # File: import_music.rb
 # Author: eweb
-# Copyright eweb, 2018-2018
+# Copyright eweb, 2018-2019
 # Contents:
 #
 # Date:          Author:  Comments:
 #  2nd Apr 2018  eweb     #0008 adding music
 # 19th Jul 2018  eweb     #0008 iTunes on Transcend drive
 #  2nd Sep 2018  eweb     #0008 sleep between moves
+#  1st Feb 2019  eweb     #0008 iTunes not on Transcend
 #
 
-itunes_dir = "#{ENV['HOME']}/Music/iTunes"
 itunes_dir = "/Volumes/Transcend/Music/iTunes"
+if !Dir.exist?(itunes_dir)
+  itunes_dir = "#{ENV['HOME']}/Music/iTunes"
+end
+
 own_dir = "#{ENV['HOME']}/Music/Own"
 downloads = "#{ENV['HOME']}/Downloads"
 
@@ -23,6 +27,7 @@ def shell(cmd)
 end
 
 Dir.chdir("#{own_dir}/temp") do
+  puts Dir["#{downloads}/*\\ -\\ *.zip"]
   shell("mv #{downloads}/*\\ -\\ *.zip .")
   shell("open *.zip")
   gets
